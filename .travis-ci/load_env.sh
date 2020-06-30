377981a9c27a8f6ba96a6acef43afe1bef7c8ac3
Pascal Havé <hpwxf@haveneer.com>
2020-06-30 16:59:45 +0200

    CICD: support for macOS and Windows

---
+++ .travis-ci/load_env.sh
@@ -0,0 +1,51 @@
+[[ "$DEBUG_CI" == true ]] && set -x
+
+case "$COMPILER" in 
+  gcc*)
+    CXX=g++${COMPILER#gcc} 
+    CC=gcc${COMPILER#gcc}
+    ;;
+  clang*)
+    CXX=clang++${COMPILER#clang} 
+    CC=clang${COMPILER#clang}
+    # initially was only for clang ≥ 7
+    CXXFLAGS="-stdlib=libc++"
+    ;;
+  msvc*)
+    #echo "${COMPILER} not supported compiler"
+    #exit 1
+    ;;
+  *)
+    echo "${COMPILER} not supported compiler"
+    exit 1
+    ;;
+esac
+
+case "$DEBUG" in 
+ true)
+    CMAKE_BUILD_TYPE="Debug"
+    ;;
+ false|"")
+     CMAKE_BUILD_TYPE="Release"
+     ;;
+  *)
+    echo "Not supported DEBUG flag [$DEBUG]"
+    exit 1
+    ;;
+esac
+
+export CXX CC CXXFLAGS CMAKE_BUILD_TYPE
+
+case $TRAVIS_OS_NAME in
+  linux|osx)
+    export PY_CMD=python$PYTHON
+    ;;
+  windows)
+    export PATH=/c/Python37:$PATH
+    export PY_CMD=python
+    ;;
+  *)
+    echo "Unknown OS [$TRAVIS_OS_NAME]"
+    exit 1
+    ;;
+esac
---
+++ .travis-ci/make_and_run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+[[ "$DEBUG_CI" == true ]] && set -x
+
+case $TRAVIS_OS_NAME in
+  linux|osx)
+    TARGET=all
+    ;;
+  windows)
+    TARGET=ALL_BUILD
+    ;;
+  *)
+    echo "Unknown OS [$TRAVIS_OS_NAME]"
+    exit 1
+    ;;
+esac
+
+# CMake configuration
+${TRAVIS_BUILD_DIR}/.travis-ci/configure.sh
+
+cd build
+
+cmake --build . --target ${TARGET} --config ${CMAKE_BUILD_TYPE}
+ctest -C ${CMAKE_BUILD_TYPE} -V
