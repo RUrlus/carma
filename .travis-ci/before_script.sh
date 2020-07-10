f4b3665833ff6d1d18ed5372b3ac3d0cb56c7344
Pascal Havé <hpwxf@haveneer.com>
2020-07-10 14:52:09 +0200

    CICD: add valgrind analysis mode and a associated build job

--- .travis-ci/before_script.sh
+++ .travis-ci/before_script.sh
@@ -2,7 +2,7 @@
 set -e
 [[ "$DEBUG_CI" == true ]] && set -x
 
-echo "PY_CMD = ${PY_CMD} [$(command -v ${PY_CMD})] [${PY_CMD} --version]"
+echo "PY_CMD = ${PY_CMD} [$(command -v ${PY_CMD})] [$(${PY_CMD} --version)]"
 echo "CXX = ${CXX} [$(command -v ${CXX})]"
 echo "CC = ${CC} [$(command -v ${CC})]"
 echo "CXXFLAGS = ${CXXFLAGS}"
