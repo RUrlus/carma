ef9155b76ed30616787c251d590eca2bd71137c3
Ralph Urlus <urlus.ralph@gmail.com>
2020-08-03 21:20:33 +0200

    CHG: Embed Python interpreter for C++ tests

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -1,7 +1,12 @@
 // In a Catch project with multiple files, dedicate one file to compile the
 // source code of Catch itself and reuse the resulting object file for linking.
 
-// Let Catch provide main():
-#define CATCH_CONFIG_MAIN
-
+#define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
+#include <pybind11/embed.h>
+
+int main( int argc, char* argv[] ) {
+    pybind11::scoped_interpreter guard{};
+    int result = Catch::Session().run( argc, argv );
+    return result;
+}
