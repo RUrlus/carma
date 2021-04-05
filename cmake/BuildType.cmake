b3dc6c1bb83c5ff4155371f85d939da8e0ca4916
Ralph Urlus <rurlus@Ralphs-MacBook-Pro.local>
2021-04-05 15:30:37 +0200

    BLD: Restructure CICD targets

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -7,3 +7,12 @@ IF (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   SET_PROPERTY(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
     "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 ENDIF ()
+
+IF (ENABLE_COVERAGE)
+    # --coverage option is used to compile and link code instrumented for coverage analysis.
+    # The option is a synonym for -fprofile-arcs -ftest-coverage (when compiling) and -lgcov (when linking).
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
+    SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
+    SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --coverage")
+ENDIF ()
