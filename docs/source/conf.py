9ebd5f4832f196032aa73dbf14908d9224e5a4ee
Ralph Urlus <rurlus.dev@gmail.com>
2021-07-19 11:13:15 +0200

    REL: Version 0.5.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, R. Urlus'
 author = 'R. Urlus'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
