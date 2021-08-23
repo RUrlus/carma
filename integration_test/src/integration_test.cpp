f108544e78e5073620318b84f6b4b542741ab94d
Ralph Urlus <rurlus.dev@gmail.com>
2021-08-23 14:02:00 +0200

    TST: Add additional test to verify linking

--- integration_test/src/integration_test.cpp
+++ integration_test/src/integration_test.cpp
@@ -12,6 +12,17 @@ void bind_test_is_f_contiguous(py::module& m) {
         "Test is F contiguous");
 }
 
+py::array_t<double> test_mat_roundtrip(py::array_t<double>& arr) {
+    // call function to be tested
+    arma::Mat<double> M = carma::arr_to_mat<double>(std::move(arr));
+    return carma::mat_to_arr(M);
+}
+
+void bind_test_mat_roundtrip(py::module& m) {
+    m.def("mat_roundtrip", &test_mat_roundtrip, "Test mat_roundtrip");
+}
+
 PYBIND11_MODULE(integration_test_carma, m) {
     bind_test_is_f_contiguous(m);
+    bind_test_mat_roundtrip(m);
 }
