bda0043e0a4fd88f85885edec5fff12769954f75
Ralph Urlus <rurlus.dev@gmail.com>
2021-08-23 14:02:00 +0200

    TST: Add additional test to verify linking

--- integration_test/tests/integration_test.py
+++ integration_test/tests/integration_test.py
@@ -13,3 +13,13 @@ def test_is_f_contiguous():
     m = 'C order array should not be F contiguous'
     sample = np.ones((10, 2), dtype=np.float64, order='C')
     assert carma.is_f_contiguous(sample) is False, m
+
+
+def test_mat_roundtrip():
+    """Test mat_roundtrip."""
+    og_sample = np.asarray(
+        np.random.normal(size=(50, 2)), dtype=np.float64, order='F'
+    )
+    sample = og_sample.copy()
+    out = carma.mat_roundtrip(sample)
+    assert np.allclose(og_sample, out)
