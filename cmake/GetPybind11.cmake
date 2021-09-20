f74fa9fcfeb593059e968ec8512452ee09ae95d4
Ralph Urlus <rurlus.dev@gmail.com>
2021-09-20 21:35:00 +0200

    DEP: Update supported Pybind11 versions to 2.7.1

--- cmake/GetPybind11.cmake
+++ cmake/GetPybind11.cmake
@@ -1,12 +1,12 @@
 include(FetchContent)
 
-SET(DEFAULT_PYBIND11_VERSION v2.6.2)
+SET(DEFAULT_PYBIND11_VERSION v2.7.1)
 IF (NOT USE_PYBIND11_VERSION)
     MESSAGE(STATUS "carma: Setting Pybind11 version to '${DEFAULT_PYBIND11_VERSION}' as none was specified.")
     SET(USE_PYBIND11_VERSION "${DEFAULT_PYBIND11_VERSION}" CACHE STRING "Choose the version of Pybind11." FORCE)
   # Set the possible values of build type for cmake-gui
   SET_PROPERTY(CACHE USE_PYBIND11_VERSION PROPERTY STRINGS
-    "v2.6.0" "v2.6.1" "v2.6.2"
+    "v2.6.0" "v2.6.1" "v2.6.2" "v2.7.1"
 )
 ENDIF ()
 
