6bf5492450980d144004097cc88077e3c4ce7a7e
Ralph Urlus <rurlus.dev@gmail.com>
2021-09-22 16:22:31 +0200

    TST: Change python command for MRE test

--- mre/pymod_test.py
+++ mre/pymod_test.py
@@ -1,7 +1,8 @@
-from build.pymod import test_runtime_error
+import build.pymod as pymod
 
-try:
-    test_runtime_error()
-except RuntimeError as e:
-    print(e)
-    print('successfully caught error')
+def test_runtime_error_exception():
+    try:
+        pymod.test_runtime_error()
+    except RuntimeError as e:
+        print(e)
+        print('successfully caught error')
