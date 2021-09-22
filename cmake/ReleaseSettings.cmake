867db2e057c4b5289ed7abd0d4da22007397a0f8
Ralph Urlus <rurlus.dev@gmail.com>
2021-09-22 10:36:13 +0200

    BLD: Only set standard for tests and examples

--- cmake/ReleaseSettings.cmake
+++
@@ -1,6 +0,0 @@
-IF (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-    # workaround like https://github.com/nlohmann/json/issues/1408
-    # to avoid error like: carma\third_party\armadillo-code\include\armadillo_bits/arma_str.hpp(194):
-    # error C2039: '_snprintf': is not a member of 'std' (compiling source file carma\tests\src\bindings.cpp) 
-    ADD_DEFINITIONS(-DHAVE_SNPRINTF)
-ENDIF ()
