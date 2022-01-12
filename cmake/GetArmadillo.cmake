80b0c051348b51fccb844d6c6560f88384872baf
Ralph Urlus <rurlus.dev@gmail.com>
2022-01-12 22:45:17 +0100

    DEP: Update default Armadillo version to 10.8.x

--- cmake/GetArmadillo.cmake
+++ cmake/GetArmadillo.cmake
@@ -1,12 +1,12 @@
 include(FetchContent)
 
-SET(DEFAULT_ARMA_VERSION 10.7.x)
+SET(DEFAULT_ARMA_VERSION 10.8.x)
 IF (NOT USE_ARMA_VERSION)
     MESSAGE(STATUS "carma: Setting Armadillo version to 'v${DEFAULT_ARMA_VERSION}' as none was specified.")
     SET(USE_ARMA_VERSION "${DEFAULT_ARMA_VERSION}" CACHE STRING "Choose the version of Armadillo." FORCE)
     # Set the possible values of build type for cmake-gui
     SET_PROPERTY(CACHE USE_ARMA_VERSION PROPERTY STRINGS
-        "10.5.x" "10.6.x" "10.7.x"
+        "10.6.x" "10.8.x"
     )
 ENDIF ()
 
