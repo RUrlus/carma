4e9da1b980a50d4b580861041c793ccf5d61acad
Ralph Urlus <rurlus.dev@gmail.com>
2022-10-05 13:39:46 +0200

    BLD: update armadillo gitlab branch to 11.4.x

--- cmake/GetArmadillo.cmake
+++ cmake/GetArmadillo.cmake
@@ -1,12 +1,12 @@
 include(FetchContent)
 
-SET(DEFAULT_ARMA_VERSION 11.2.x)
+SET(DEFAULT_ARMA_VERSION 11.4.x)
 IF (NOT USE_ARMA_VERSION)
     MESSAGE(STATUS "carma: Setting Armadillo version to 'v${DEFAULT_ARMA_VERSION}' as none was specified.")
     SET(USE_ARMA_VERSION "${DEFAULT_ARMA_VERSION}" CACHE STRING "Choose the version of Armadillo." FORCE)
     # Set the possible values of build type for cmake-gui
     SET_PROPERTY(CACHE USE_ARMA_VERSION PROPERTY STRINGS
-        "10.6.x" "10.8.x" "11.2.x"
+        "10.6.x" "10.8.x" "11.2.x" "11.4.x"
     )
 ENDIF ()
 
