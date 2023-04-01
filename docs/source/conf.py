8405bafc40b1bf06795e7004c7898a08963b688c
Ralph Urlus <rurlus.dev@gmail.com>
2023-04-01 13:08:17 +0200

    REL: Release of v0.6.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,18 +11,19 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 sys.setrecursionlimit(1500)
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'carma'
-copyright = '2020, R. Urlus'
-author = 'R. Urlus'
+project = "carma"
+copyright = "2020, R. Urlus"
+author = "R. Urlus"
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = "0.6.7"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,16 +32,16 @@ release = '0.6.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.coverage',
-    'sphinx.ext.ifconfig',
-    'sphinxcontrib.napoleon',
-    'sphinx.ext.autosectionlabel',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.ifconfig",
+    "sphinxcontrib.napoleon",
+    "sphinx.ext.autosectionlabel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,11 +55,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 import sphinx_rtd_theme
+
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_options = {
-    'logo_only': True
-}
+html_theme_options = {"logo_only": True}
 html_logo = "carma_logo.png"
 html_show_sourcelink = True
 
@@ -66,6 +66,6 @@ html_show_sourcelink = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-master_doc = 'index'
+master_doc = "index"
 
 # -- Extension configuration -------------------------------------------------
